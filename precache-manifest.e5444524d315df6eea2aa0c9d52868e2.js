56dd24bdd1c60d3fc7e5408bb00eb19cfccd2ba5
Aakash152 <aakashkharche152@gmail.com>
2020-06-09 19:53:13 +0530

    Updates

---
+++ precache-manifest.e5444524d315df6eea2aa0c9d52868e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8514213bfa6a64b9420e54a63fd50ef",
+    "url": "/tvlist/index.html"
+  },
+  {
+    "revision": "587f5690f08a43c361b8",
+    "url": "/tvlist/static/css/main.6e11845f.chunk.css"
+  },
+  {
+    "revision": "58245534cb4432635bec",
+    "url": "/tvlist/static/js/2.84cc2c15.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tvlist/static/js/2.84cc2c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587f5690f08a43c361b8",
+    "url": "/tvlist/static/js/main.ef0262b8.chunk.js"
+  },
+  {
+    "revision": "9bb6e46d4a54d13c7f35",
+    "url": "/tvlist/static/js/runtime-main.6e428e57.js"
+  }
+]);

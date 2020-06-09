8abe1a1e48dea015eef26677737040e37df1a5ee
Aakash152 <aakashkharche152@gmail.com>
2020-06-09 21:05:00 +0530

    fff

--- src/Components/Entry.js
+++ src/Components/Entry.js
@@ -2,16 +2,19 @@ import React from "react";
 
 import Series from "../Containers/Series";
 import SingleSeries from "../Containers/SingleSeries";
+import { BrowserRouter } from "react-router-dom";
 
 import { Switch, Route } from "react-router-dom";
 
 const Entry = () => {
   return (
     <div>
+       <BrowserRouter basename="/tvlist">
       <Switch>
         <Route exact path="/" component={Series} />
         <Route exact path="/series/:id" component={SingleSeries} />
       </Switch>
+      </BrowserRouter>
     </div>
   );
 };
